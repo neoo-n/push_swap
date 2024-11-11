eaac6c606ab09f82b9ca9c8f33d42adbc6ffe5db
Dana Vauthey <dvauthey@c2r4s3.42lausanne.ch>
2024-11-11 11:59:22 +0100

    chained list to table

--- ps_ops.c
+++ ps_ops.c
@@ -6,56 +6,26 @@
 /*   By: dvauthey <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 18:05:27 by dvauthey          #+#    #+#             */
-/*   Updated: 2024/11/08 18:15:56 by dvauthey         ###   ########.fr       */
+/*   Updated: 2024/11/11 10:58:37 by dvauthey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	*fct_sa(int *a)
+int	*fct_sab(int *a)
 {
 	int	len;
-	t_stack	temp;
+	int	temp;
 
-	len = ft_lstsize(a);
+	len = ft_(a);
 	if (len < 2)
 		return (a);
-	temp = (t_stack)ft_calloc(1, sizeof(t_stack));
-	ft_lstadd_back(temp, a->next);
-	ft_lstadd_back(temp, a);
-	a = a->next;
-	a = a->next;
-	while (a)
-	{
-		ft_lstadd_back(temp, a);
-		a = a->next;
-	}
-	free(a);
-	return (temp);
-}
-
-int	*fct_sb(int *b)
-{
-	int	len;
-	t_stack	temp;
-
-	len = ft_lstsize(b);
-	if (len < 2)
-		return (b);
-	temp = (t_stack)ft_calloc(1, sizeof(t_stack));
-	ft_lstadd_back(temp, b->next);
-	ft_lstadd_back(temp, b);
-	b = b->next;
-	b = b->next;
-	while (b)
-	{
-		ft_lstadd_back(temp, b);
-		b = b->next;
-	}
-	free(b);
+	temp = a[0];
+	a[0] = a[1];
+	a[1] = temp;
 	return (temp);
 }
 
 void	fct_ss(int *a, int *b)
 {
-	fct_sa(a);
-	fct_sb(b);
+	fct_sab(a);
+	fct_sab(b);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,18 +6,11 @@
 /*   By: dvauthey <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 17:56:47 by dvauthey          #+#    #+#             */
-/*   Updated: 2024/11/08 17:57:16 by dvauthey         ###   ########.fr       */
+/*   Updated: 2024/11/11 10:54:07 by dvauthey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
-typedef struct s_stack
-{
-	int	number;
-	t_stack	*previous;
-	t_stack	*next;
-}		t_stack;
 
 #endif
